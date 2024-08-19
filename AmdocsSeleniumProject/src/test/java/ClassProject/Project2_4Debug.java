3550f92516a4a39ba6df0da0a6f1592f7262c26a
Asmi Vig <80142487+asmi02@users.noreply.github.com>
2024-08-19 10:17:33 +0530

    Update Project2_4Debug.java

--- AmdocsSeleniumProject/src/test/java/ClassProject/Project2_4Debug.java
+++ AmdocsSeleniumProject/src/test/java/ClassProject/Project2_4Debug.java
@@ -51,61 +51,61 @@ public class Project2_4Debug
             driver.quit();
         }
     }
-    @Test
-    public void demo3()
-    {
-        // Set up the WebDriver and open the browser
-        System.setProperty("webdriver.chrome.driver","D:\\downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        try
-        {
-            driver.manage().window().maximize();
-            driver.get("https://www.selenium.dev/");
-            WebElement l1 = driver.findElement(By.linkText("Documentation"));
-            l1.click();
-
-            WebElement text1 = driver.findElement(By.className("DocSearch-Button-Placeholder"));
-            text1.click();
-            WebElement text2 = driver.findElement(By.className("DocSearch-Input"));
-            text2.sendKeys("Selenium IDE");
-            text2.click();
-            Actions builder = new Actions(driver);
-            builder.moveToElement(text2).sendKeys(Keys.DOWN).sendKeys(Keys.ENTER);
-            Thread.sleep(2000);
-        }
-
-        catch (Exception ex)
-        {
-            ex.printStackTrace();
-            driver.quit();
-        }
-    }
-    @Test
-    public void demo4()
-    {
-        // Set up the WebDriver and open the browser
-        System.setProperty("webdriver.chrome.driver","D:\\downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        try
-        {
-            driver.manage().window().maximize();
-            driver.get("https://www.selenium.dev/");
-            WebElement l1 = driver.findElement(By.linkText("Documentation"));
-            l1.click();
-
-            WebElement text1 = driver.findElement(By.className("DocSearch-Button-Placeholder"));
-            text1.click();
-            WebElement text2 = driver.findElement(By.className("DocSearch-Input"));
-            text2.sendKeys("");
-            text2.click();
-            Actions builder = new Actions(driver);
-            builder.moveToElement(text2).sendKeys(Keys.DOWN).sendKeys(Keys.ENTER);
-        }
-
-        catch (Exception ex)
-        {
-            ex.printStackTrace();
-            driver.quit();
-        }
-    }
+//    @Test
+//    public void demo3()
+//    {
+//        // Set up the WebDriver and open the browser
+//        System.setProperty("webdriver.chrome.driver","D:\\downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+//        WebDriver driver = new ChromeDriver();
+//        try
+//        {
+//            driver.manage().window().maximize();
+//            driver.get("https://www.selenium.dev/");
+//            WebElement l1 = driver.findElement(By.linkText("Documentation"));
+//            l1.click();
+//
+//            WebElement text1 = driver.findElement(By.className("DocSearch-Button-Placeholder"));
+//            text1.click();
+//            WebElement text2 = driver.findElement(By.className("DocSearch-Input"));
+//            text2.sendKeys("Selenium IDE");
+//            text2.click();
+//            Actions builder = new Actions(driver);
+//            builder.moveToElement(text2).sendKeys(Keys.DOWN).sendKeys(Keys.ENTER);
+//            Thread.sleep(2000);
+//        }
+//
+//        catch (Exception ex)
+//        {
+//            ex.printStackTrace();
+//            driver.quit();
+//        }
+//    }
+//    @Test
+//    public void demo4()
+//    {
+//        // Set up the WebDriver and open the browser
+//        System.setProperty("webdriver.chrome.driver","D:\\downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+//        WebDriver driver = new ChromeDriver();
+//        try
+//        {
+//            driver.manage().window().maximize();
+//            driver.get("https://www.selenium.dev/");
+//            WebElement l1 = driver.findElement(By.linkText("Documentation"));
+//            l1.click();
+//
+//            WebElement text1 = driver.findElement(By.className("DocSearch-Button-Placeholder"));
+//            text1.click();
+//            WebElement text2 = driver.findElement(By.className("DocSearch-Input"));
+//            text2.sendKeys("");
+//            text2.click();
+//            Actions builder = new Actions(driver);
+//            builder.moveToElement(text2).sendKeys(Keys.DOWN).sendKeys(Keys.ENTER);
+//        }
+//
+//        catch (Exception ex)
+//        {
+//            ex.printStackTrace();
+//            driver.quit();
+//        }
+//    }
 }
